75c959336fb1aecf44cda0d197662b702aac4433
lindyhopfan <josh.sarean@gmail.com>
2019-07-22 00:11:26 -0700

    local jquery ui

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
-let year = $( "#year" ).val();
-console.log("year", year);
+jQuery( document ).ready(function( $ ) {
+  $( function() {
+    $( "#tabs" ).tabs();
+    $( "#slider-vertical" ).slider({
+      orientation: "vertical",
+      range: "false",
+      animate: "fast",
+      min: 1949,
+      max: 2019,
+      value: 1984,
+      slide: function( event, ui ) {
+        $( "#year" ).val( ui.value );
+      }
+    });
+    $( "#year" ).val( $( "#slider-vertical" ).slider( "value" ) );
+  } );
+});
