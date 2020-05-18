8966a07435b4ad0f11b842c8b199b19b30f1a75d
lindyhopfan <josh.sarean@gmail.com>
2020-05-18 00:32:22 -0700

    featured playlists

--- js/playlists.js
+++ js/playlists.js
@@ -2509,6 +2509,16 @@ var playlists = {
     "year": "featured",
     "genre": "Swing-a-Billy Gospel"
   },
+  "0L5m4357HFIF6I4Jkqy8Jm": {
+    "code": "0L5m4357HFIF6I4Jkqy8Jm",
+    "year": "featured",
+    "genre": "Songs and Creations"
+  },
+  "29nXAvWhkGDcGzObEXOrxP": {
+    "code": "29nXAvWhkGDcGzObEXOrxP",
+    "year": "featured",
+    "genre": "Hymns Vol 1: Living Hymns Extended"
+  },
   "1FosAaaKAiHWrNgt5kLc1B": {
     "code": "1FosAaaKAiHWrNgt5kLc1B",
     "year": "christianRockPioneers",
@@ -2689,11 +2699,6 @@ var playlists = {
     "year": "hymnsAndSongbooks",
     "genre": "Hymns For Christmas"
   },
-  "0L5m4357HFIF6I4Jkqy8Jm": {
-    "code": "0L5m4357HFIF6I4Jkqy8Jm",
-    "year": "hymnsAndSongbooks",
-    "genre": "Songs and Creations"
-  },
   "7hKkVUavwYAOaihEfDhDMo": {
     "code": "7hKkVUavwYAOaihEfDhDMo",
     "year": "ultimatePlaylists",
@@ -2738,60 +2743,5 @@ var playlists = {
     "code": "0MkWMg2neAOup7rlOirC7e",
     "year": "ultimatePlaylists",
     "genre": "Ultimate HIPSTER & INDIE"
-  },
-  "3Mg5dSok09Crpvyn6jcfMP": {
-    "code": "3Mg5dSok09Crpvyn6jcfMP",
-    "year": "secularPlaylists",
-    "genre": "The Dancing Habit"
-  },
-  "3oHajqFcGyOZnN9QGosUXh": {
-    "code": "3oHajqFcGyOZnN9QGosUXh",
-    "year": "secularPlaylists",
-    "genre": "Halloween Swing"
-  },
-  "6CJkyImIaN0cEUY0Z6a3Eh": {
-    "code": "6CJkyImIaN0cEUY0Z6a3Eh",
-    "year": "secularPlaylists",
-    "genre": "Best Emo Songs of All Time"
-  },
-  "16s9dae8iRc5Ls01VYQEl8": {
-    "code": "16s9dae8iRc5Ls01VYQEl8",
-    "year": "secularPlaylists",
-    "genre": "Ye Olde Emo"
-  },
-  "16qpFldOvBtG35YWNsJMGa": {
-    "code": "16qpFldOvBtG35YWNsJMGa",
-    "year": "secularPlaylists",
-    "genre": "700+ Emo and Pop Punk Bands"
-  },
-  "3vxotOnOGDlZXyzJPLFnm2": {
-    "code": "3vxotOnOGDlZXyzJPLFnm2",
-    "year": "secularPlaylists",
-    "genre": "Hipster International"
-  },
-  "4PvRqEu0fcA3nhEgesbAg5": {
-    "code": "4PvRqEu0fcA3nhEgesbAg5",
-    "year": "secularPlaylists",
-    "genre": "The Hipster Housewife"
-  },
-  "3dwjG5KzlvhQKv8Fp03xL9": {
-    "code": "3dwjG5KzlvhQKv8Fp03xL9",
-    "year": "secularPlaylists",
-    "genre": "A Hipster's Guide To Sleep"
-  },
-  "1gKxwjTsqpRwR1VMxwwsqe": {
-    "code": "1gKxwjTsqpRwR1VMxwwsqe",
-    "year": "secularPlaylists",
-    "genre": "SKA: First Wave and Rocksteady"
-  },
-  "4qbmqyYnmImylRe4mllj02": {
-    "code": "4qbmqyYnmImylRe4mllj02",
-    "year": "secularPlaylists",
-    "genre": "SKA: Second Wave and Two Tone"
-  },
-  "48091ieG81bG7mwg4gr985": {
-    "code": "48091ieG81bG7mwg4gr985",
-    "year": "secularPlaylists",
-    "genre": "Old Time Religion"
   }
 };
---
+++ js/secular.js
@@ -0,0 +1,57 @@
+var playlists = {
+  "3Mg5dSok09Crpvyn6jcfMP": {
+    "code": "3Mg5dSok09Crpvyn6jcfMP",
+    "year": "secularPlaylists",
+    "genre": "The Dancing Habit"
+  },
+  "3oHajqFcGyOZnN9QGosUXh": {
+    "code": "3oHajqFcGyOZnN9QGosUXh",
+    "year": "secularPlaylists",
+    "genre": "Halloween Swing"
+  },
+  "6CJkyImIaN0cEUY0Z6a3Eh": {
+    "code": "6CJkyImIaN0cEUY0Z6a3Eh",
+    "year": "secularPlaylists",
+    "genre": "Best Emo Songs of All Time"
+  },
+  "16s9dae8iRc5Ls01VYQEl8": {
+    "code": "16s9dae8iRc5Ls01VYQEl8",
+    "year": "secularPlaylists",
+    "genre": "Ye Olde Emo"
+  },
+  "16qpFldOvBtG35YWNsJMGa": {
+    "code": "16qpFldOvBtG35YWNsJMGa",
+    "year": "secularPlaylists",
+    "genre": "700+ Emo and Pop Punk Bands"
+  },
+  "3vxotOnOGDlZXyzJPLFnm2": {
+    "code": "3vxotOnOGDlZXyzJPLFnm2",
+    "year": "secularPlaylists",
+    "genre": "Hipster International"
+  },
+  "4PvRqEu0fcA3nhEgesbAg5": {
+    "code": "4PvRqEu0fcA3nhEgesbAg5",
+    "year": "secularPlaylists",
+    "genre": "The Hipster Housewife"
+  },
+  "3dwjG5KzlvhQKv8Fp03xL9": {
+    "code": "3dwjG5KzlvhQKv8Fp03xL9",
+    "year": "secularPlaylists",
+    "genre": "A Hipster's Guide To Sleep"
+  },
+  "1gKxwjTsqpRwR1VMxwwsqe": {
+    "code": "1gKxwjTsqpRwR1VMxwwsqe",
+    "year": "secularPlaylists",
+    "genre": "SKA: First Wave and Rocksteady"
+  },
+  "4qbmqyYnmImylRe4mllj02": {
+    "code": "4qbmqyYnmImylRe4mllj02",
+    "year": "secularPlaylists",
+    "genre": "SKA: Second Wave and Two Tone"
+  },
+  "48091ieG81bG7mwg4gr985": {
+    "code": "48091ieG81bG7mwg4gr985",
+    "year": "secularPlaylists",
+    "genre": "Old Time Religion"
+  }
+};
